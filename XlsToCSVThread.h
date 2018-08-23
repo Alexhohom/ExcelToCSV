7f3bff9bd2913b327ce19722e0e02fdf0a3ded2d
alexhohom <alexhohom@gmail.com>
2018-08-23 16:50:10 +0800

    Recover

--- XlsToCSVThread.h
+++ XlsToCSVThread.h
@@ -13,7 +13,7 @@ private:
 protected:
 	void __fastcall Execute();
 public:
-	__fastcall TXlsToCSVThread(bool CreateSuspended, String sFile);daksdakshd
+	__fastcall TXlsToCSVThread(bool CreateSuspended, String sFile);
 };
 //---------------------------------------------------------------------------
 #endif
